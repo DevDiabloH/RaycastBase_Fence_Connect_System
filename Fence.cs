128fa4f459be9e3e9dbb99e1c6a25f14a8f3cda5
DevDiabloH <50089624+DevDiabloH@users.noreply.github.com>
2020-01-06 13:23:13 +0900

    init

---
+++ Fence.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+public enum EDirection { Forward, Back, Right, Left }
+
+[Serializable]
+public class Fence
+{
+    public EDirection direction;
+    public Transform transform;
+    public bool isConnected
+    {
+        get
+        {
+            return m_IsConnected;
+        }
+        set
+        {
+            m_IsConnected = value;
+
+            if(null != transform)
+            {
+                transform.gameObject.SetActive(m_IsConnected);
+            }
+            else
+            {
+                //Debug.LogWarning(direction + " object transform is null.");
+            }
+        }
+    }
+    private bool m_IsConnected;
+
+    public Fence(EDirection direction, Transform transform)
+    {
+        this.direction = direction;
+        this.transform = transform;
+        this.isConnected = false;
+    }
+
+    public Vector3 GetVector()
+    {
+        switch (direction)
+        {
+            case EDirection.Forward:
+                return Vector3.forward;
+            case EDirection.Back:
+                return Vector3.back;
+            case EDirection.Right:
+                return Vector3.right;
+            case EDirection.Left:
+                return Vector3.left;
+            default:
+                return Vector3.zero;
+        }
+    }
+}
---
+++ FenceConnector.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+
+public class FenceConnector : MonoBehaviour
+{
+    private Fence[] m_Fences;
+    private LayerMask m_Mask;
+    private readonly string m_LayerMaskName = "Fence";
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Start()
+    {
+        FindNearbyFence();
+    }
+
+    private void OnDisable()
+    {
+        FindNearbyFence(false);
+    }
+
+    private void Initialize()
+    {
+        m_Fences = new Fence[Enum.GetNames(typeof(EDirection)).Length];
+
+        try
+        {
+            m_Mask = 1 << LayerMask.NameToLayer(m_LayerMaskName);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+
+        for (int i=0; i<m_Fences.Length; i++)
+        {
+            EDirection _dir = (EDirection)i;
+
+            if(null == this.transform.Find(_dir.ToString()))
+            {
+                //Debug.LogError("The fence must have " + m_Fences.Length + " directions, " +
+                //    "There is no object in the " + _dir.ToString() + " direction.");
+                m_Fences[i] = new Fence(_dir, null);
+            }
+            else
+            {
+                m_Fences[i] = new Fence(_dir, this.transform.Find(_dir.ToString()));
+            }
+        }
+    }
+
+    private void FindNearbyFence(bool isEnqble=true)
+    {
+        for(int i=0; i<m_Fences.Length; i++)
+        {
+            Vector3 origin = this.transform.position;
+            Vector3 direction = m_Fences[i].GetVector();
+            RaycastHit hit;
+            
+            if (Physics.Raycast(origin, direction, out hit, 1f, m_Mask))
+            {
+                //Debug.DrawRay(origin + (Vector3.up * 0.5f), direction, Color.green, 3f);
+                Fence instance = hit.transform.GetComponent<FenceConnector>().GetRequest(direction);
+
+                if(null != instance.transform)
+                {
+                    instance.isConnected = isEnqble;
+                    m_Fences[i].isConnected = isEnqble;
+                }
+            }
+            else
+            {
+                //Debug.DrawRay(origin + (Vector3.up * 0.5f), direction, Color.red, 3f);
+                m_Fences[i].isConnected = false;
+            }
+        }
+    }
+
+    public Fence GetRequest(Vector3 directionVector)
+    {
+        if(null == m_Fences)
+        {
+            Initialize();
+        }
+
+        for(int i=0; i<m_Fences.Length; i++)
+        { 
+            if(-m_Fences[i].GetVector() == directionVector)
+            {
+                return m_Fences[i];
+            }
+        }
+
+        return null;
+    }
+}
